5dbf79882039e6f3be4e84ca8e837b985093fcc8
Lars Trieloff <trieloff@adobe.com>
2020-03-20 15:45:29 +0000

    refactor(index): basic framework to support multiple embedders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Adobe. All rights reserved.
+ * Copyright 2020 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -13,16 +13,30 @@ const { wrap } = require('@adobe/openwhisk-action-utils');
 const { logger } = require('@adobe/openwhisk-action-logger');
 const { wrap: status } = require('@adobe/helix-status');
 const { epsagon } = require('@adobe/helix-epsagon');
+const embed = require('./embed');
 
-/**
- * This is the main function
- * @param {string} name name of the person to greet
- * @returns {object} a greeting
- */
-function main({ name = 'world' }) {
-  return {
-    body: `Hello, ${name}.`,
-  };
+async function main(params) {
+  const { __ow_logger: log = console } = params;
+
+  if (!params.__ow_query) {
+    // reconstruct __ow_query
+    const query = Object.keys(params)
+      .filter((key) => !/^[A-Z]+_[A-Z]+/.test(key))
+      .filter((key) => key !== 'api')
+      .filter((key) => !/^__ow_/.test(key))
+      .reduce((pv, cv) => {
+        if (pv) {
+          return `${pv}&${cv}=${params[cv]}`;
+        }
+        return `${cv}=${params[cv]}`;
+      }, '');
+    // eslint-disable-next-line no-param-reassign
+    params.__ow_query = query;
+  }
+  const url = `${params.__ow_path.substring(1)}?${params.__ow_query || ''}`;
+
+  const result = await embed(url, params, log);
+  return result;
 }
 
 module.exports.main = wrap(main)
