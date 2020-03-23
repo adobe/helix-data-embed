96f2a5d8a3f54fd9e49110aa2dbc09d1d6f9ded6
Lars Trieloff <trieloff@adobe.com>
2020-03-23 15:05:28 +0000

    test(post-deploy): add post-deploy test

---
+++ test/post-deploy.test.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const packjson = require('../package.json');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+function getbaseurl() {
+  const namespace = 'helix';
+  const package = 'helix-services-private';
+  const name = packjson.name.replace('@adobe/helix-', '');
+  let version = `${packjson.version}`;
+  if (process.env.CI && process.env.CIRCLE_BUILD_NUM && process.env.CIRCLE_BRANCH !== 'master') {
+    version = `ci${process.env.CIRCLE_BUILD_NUM}`;
+  }
+  return `api/v1/web/${namespace}/${package}/${name}@${version}`;
+}
+
+describe('Post-Deploy Tests', () => {
+  it('RSS Embed', async () => {
+    await chai
+      .request('https://adobeioruntime.net/')
+      .get(`${getbaseurl()}/https://blogs.adobe.com/psirt/?feed=atom`)
+      .then((response) => {
+        expect(response).to.have.status(200);
+        expect(response).to.be.json;
+        console.log(response.json);
+      }).catch((e) => {
+        throw e;
+      });
+  });
+});
