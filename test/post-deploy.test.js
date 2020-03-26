0b0585be065072e654d89d31ad34fa9af66dbc89
Lars Trieloff <trieloff@adobe.com>
2020-03-26 12:38:23 +0000

    test(post-deploy): increase timeouts for slower XLSX processing

--- test/post-deploy.test.js
+++ test/post-deploy.test.js
@@ -43,7 +43,7 @@ describe('Post-Deploy Tests', () => {
       }).catch((e) => {
         throw e;
       });
-  });
+  }).timeout(10000);
 
   it('Excel Embed', async () => {
     await chai
@@ -59,5 +59,5 @@ describe('Post-Deploy Tests', () => {
       }).catch((e) => {
         throw e;
       });
-  });
+  }).timeout(10000);
 });
