b4264257a1ba3d7174a221bb83a5eb378c49aabd
Lars Trieloff <trieloff@adobe.com>
2020-05-18 12:44:21 +0000

    test(post-deploy): use different spreadsheet for post-deploy test

--- test/post-deploy.test.js
+++ test/post-deploy.test.js
@@ -46,18 +46,17 @@ describe('Post-Deploy Tests', () => {
   }).timeout(10000);
 
   it('Excel Embed', async () => {
-    console.log('Trying', `https://adobeioruntime.net/${getbaseurl()}/https://adobe.sharepoint.com/sites/TheBlog/_layouts/15/guestaccess.aspx?share=ESR1N29Z7HpCh1Zfs_0YS_gB4gVSuKyWRut-kNcHVSvkew&email=helix%40adobe.com&e=hx0OUl`);
+    console.log('Trying', `https://adobe-my.sharepoint.com/personal/trieloff_adobe_com/_layouts/15/guestaccess.aspx?share=Edoi88tLKLpDsKzSfL-pcJYB2lIo7UKooYWnjm3w2WRrsA&email=helix%40adobe.com&e=tD623x`);
 
     await chai
       .request('https://adobeioruntime.net/')
-      .get(`${getbaseurl()}/https://adobe.sharepoint.com/sites/TheBlog/_layouts/15/guestaccess.aspx?share=ESR1N29Z7HpCh1Zfs_0YS_gB4gVSuKyWRut-kNcHVSvkew&email=helix%40adobe.com&e=hx0OUl`)
+      .get(`${getbaseurl()}/https://adobe-my.sharepoint.com/personal/trieloff_adobe_com/_layouts/15/guestaccess.aspx?share=Edoi88tLKLpDsKzSfL-pcJYB2lIo7UKooYWnjm3w2WRrsA&email=helix%40adobe.com&e=tD623x`)
       .then((response) => {
         // console.log(response.body);
         expect(response).to.be.json;
         expect(response.body).to.be.an('array').that.deep.includes({
-          'import date': '2020-04-23T12:55:40.852Z',
-          url: 'https://theblog.adobe.com/best-practices-in-content-management-it-edition/',
-          year: 43875,
+          project: 'Helix',
+          created: 2018,
         });
         expect(response).to.have.status(200);
       }).catch((e) => {
