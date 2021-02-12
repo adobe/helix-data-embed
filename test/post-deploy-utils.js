983df9b31a846d575e1ab4d96027c19fbb14d626
Stefan Guggisberg <sguggisb@adobe.com>
2021-02-12 12:23:46 +0100

    test: make lint shutup

--- test/post-deploy-utils.js
+++ test/post-deploy-utils.js
@@ -47,18 +47,6 @@ class OpenwhiskTarget {
   }
 }
 
-class OpenwhiskPreprodTarget extends OpenwhiskTarget {
-  // eslint-disable-next-line class-methods-use-this
-  title() {
-    return 'OpenWhisk - Preprod';
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  host() {
-    return 'https://preprod.adobeioruntime.net';
-  }
-}
-
 class AWSTarget extends OpenwhiskTarget {
   // eslint-disable-next-line class-methods-use-this
   title() {
