8142dd15c31d9d5f2cac7e99b9d9f3bd7d1294e0
Tobias Bocanegra <tripod@adobe.com>
2021-04-13 13:56:47 +0900

    fix: use helix-universal

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const { Response } = require('@adobe/helix-fetch');
-const { wrap } = require('@adobe/openwhisk-action-utils');
-const { logger } = require('@adobe/openwhisk-action-logger');
+const { Response } = require('@adobe/helix-universal');
+const { wrap } = require('@adobe/helix-shared');
+const { logger } = require('@adobe/helix-universal-logger');
 const { wrap: helixStatus } = require('@adobe/helix-status');
 const embed = require('./embed');
 const { loadquerystring } = require('./querybuilder/url');
